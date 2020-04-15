3b26c589997f6466af0e32376068c0db4d0cfae7
Leirton <mttomleirton@gmail.com>
2020-04-15 19:28:25 -0300

    ending the 1st version

--- automatizar.sh
+++ automatizar.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 
 count=0
-number_of_word=5
+number_of_word=2
 
 declare -a search_word
 declare -a rest_word
 
-  #anki &
-  # google-chrome --new-window "https://www.google.com/search?tbm=isch&q=$line" "https://context.reverso.net/traducao/ingles-portugues/$line"
+anki >& message_anki.txt &
 
 #Separating the words that will be search 
  while read line
@@ -21,19 +20,25 @@ declare -a rest_word
     count=$(( count+1 ))
 done < "${1:-/dev/stdin}"
 
-
+#the searched words in the your file. 
 echo >> pesquisadas
 echo "$(date +"%d/%m/%y")" >> pesquisadas
 echo >> pesquisadas
+
 for word in ${search_word[*]}
 do
-  echo "$word - OK" >> "pesquisadas"
+  google-chrome --new-window "https://www.google.com/search?tbm=isch&q=$word" "https://context.reverso.net/traducao/ingles-portugues/$word" >& message_google.txt
+
+  echo "a palavra --$word-- foi inserida no anki?"
+  read ok;
+  echo "$word - $ok" >> "pesquisadas"
 done
 
-
+: '
+#salving the file with the words not searched
 > para_pesquisar
 for word in ${rest_word[*]}
 do
   echo "$word" >> "para_pesquisar"
 done
-
+'
